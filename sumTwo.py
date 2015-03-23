0a0d7c8f61ebf022bac981900c740a5526e48ea9
Caster.Rune <caster.v.rune@gmail.com>
2015-03-23 22:10:09 +0800

    sumTwo

--- sumTwo.py
+++ sumTwo.py
@@ -1,1 +1,19 @@
-__author__ = 'Rune'
+class Solution:
+    def twoSum(num, target):
+        t = {}
+        for i, n in enumerate(num):
+            t[n] = i
+        print(t)
+        for i, n in enumerate(num):
+            a = target - n
+            if a in t:
+                b = t[a]
+                if i < b:
+                    return (i+1, b+1)
+                elif b < i:
+                    return (b+1, i+1)
+
+if __name__ == '__main__':
+    num = {1, 2, 5, 9}
+    target = 7
+    print(Solution.twoSum(num, target))
